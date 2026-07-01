28b21635566aeaf9de80796bfab5d23c325e45b2
Rikusu Shado <rikusushado@proton.me>
2026-07-01 14:09:33 +0200

    WIP: Location Assistant

--- framewase-website-manager/cmake/install-rules.cmake
+++
@@ -1,8 +0,0 @@
-install(
-    TARGETS framewase-website-manager_exe
-    RUNTIME COMPONENT framewase-website-manager_Runtime
-)
-
-if(PROJECT_IS_TOP_LEVEL)
-  include(CPack)
-endif()
--- framewase-website-manager/source/input/setup.h
+++
@@ -1,5 +0,0 @@
-#include <gtk/gtk.h>
-
-typedef struct {
-    gdouble slider_step_size;
-} InputOptions;
